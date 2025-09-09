2d0aadc54793effc8e12ef9d5a2616f579da238b
LEOVBOX <75016292+LEOVBOX@users.noreply.github.com>
2025-09-09 10:54:46 +0700

    Initial Commit

---
+++ golfpad_test/golfpad_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  golfpad_testApp.swift
+//  golfpad_test
+//
+//  Created by Леонид Шайхутдинов on 09.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct golfpad_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
